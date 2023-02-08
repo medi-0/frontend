9832835046cd8e8309866afc2e0d3ae8ea7f65a2
gavinowen99 <gavinowen713@gmail.com>
2023-02-08 14:09:30 +0800

    verify panel
    verify panel

--- src/pages/Verifier.tsx
+++ src/pages/Verifier.tsx
@@ -1,20 +1,38 @@
-import { VerifyForm } from "../components/verifier/VerifyProof";
 import React, { useState } from "react";
-import VerifyQr from "../components/verifier/VerifyQr";
+import Navbar from "../components/navbar/Navbar";
+import { useAccount } from "wagmi";
+import { Center, Text } from "@chakra-ui/react";
+import TabModal from "../components/verifier/TabModal";
 
 export default function Verifier() {
-  
+  const { address, isConnected, connector } = useAccount();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  if (!isConnected)
+    return (
+      <div>
+        <Navbar />
+        <div className="m-10">
+          <h1 className="mx-10 text-4xl font-bold text-[#3C84AB] ">
+            Verify Proof
+          </h1>
+          <hr className="mt-2 mb-10" />
+          <Center>
+            <Text>Please Connect wallet to proceed</Text>
+          </Center>
+        </div>
+      </div>
+    );
   return (
     <div>
-      <div className="m-10 rounded-sm ">
+      <Navbar />
+      <div className="m-10">
         <h1 className="mx-10 text-4xl font-bold text-[#3C84AB] ">
           Verify Proof
         </h1>
         <hr className="mt-2 mb-10" />
-
-        <VerifyForm />
-        <VerifyQr/>
-
+        <TabModal/>
+        
       </div>
     </div>
   );
